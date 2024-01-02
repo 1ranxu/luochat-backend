62a2a8d5320bcfa883fb72038041f2652bd5a558
1ranxu <263989312@qq.com>
2024-01-02 11:43:09 +0800

    feat：netty心跳原理

--- luochat-chat-server/src/main/java/com/luoying/luochat/common/websocket/NettyWebSocketServerHandler.java
+++ luochat-chat-server/src/main/java/com/luoying/luochat/common/websocket/NettyWebSocketServerHandler.java
@@ -8,6 +8,8 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -21,8 +23,15 @@ public class NettyWebSocketServerHandler extends SimpleChannelInboundHandler<Tex
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
         // 监听握手完成事件
-        if(evt instanceof WebSocketServerProtocolHandler.HandshakeComplete){
+        if (evt instanceof WebSocketServerProtocolHandler.HandshakeComplete) {
             System.out.println("握手完成");
+        } else if (evt instanceof IdleStateEvent) {
+            IdleStateEvent event = (IdleStateEvent) evt;
+            if (event.state() == IdleState.READER_IDLE) {
+                System.out.println("读空闲");
+                // todo 用户下线
+                ctx.channel().close();
+            }
         }
     }
 
